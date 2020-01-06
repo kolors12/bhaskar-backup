da801c87522881d93c48a92c45dcb1e87a22de38
Suggu Bhaskar Rao <SB591114@GSK.SATYAMBPO.COM>
2020-01-06 20:39:40 +0530

    remove index

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/bhaskar-backup/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
